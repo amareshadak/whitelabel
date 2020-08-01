d7648bb304146abd7ea79f7ba3a0d56c3f7b60a2
rasabless2020 <rahuls1017@gmail.com>
2020-08-01 14:56:18 +0530

    Ref

---
+++ WHITELABEL.Web/obj/Release/Package/PackageTmp/Areas/Merchant/MerchantHelperJS/ElectricityBillPayment/Index.js
@@ -0,0 +1,105 @@
+﻿$(function () {
+    $("#txtElectricityOperator").autocomplete({
+        source: function (request, response) {
+            var OperatorType = "ELECTRICITY";
+            $.ajax({
+                url: '/MerchantRechargeService/AutoElectricityBillService',
+                data: "{ 'prefix': '" + request.term + "'}",
+                dataType: "json",
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return item;
+                    }))
+                },
+                error: function (response) {
+                    alert(response.responseText);
+                },
+                failure: function (response) {
+                    alert(response.responseText);
+                }
+            });
+        },
+        focus: function (event, ui) {
+            // this is for when focus of autocomplete item 
+            $('#txtElectricityOperator').val(ui.item.label);
+            return false;
+        },
+        select: function (e, i) {
+            $("#hfElectricityperator").val(i.item.val);
+            return true ;
+        },
+        minLength: 1
+    }).data("ui-autocomplete")._renderItem = function (ul, item) {
+        return $("<li></li>")
+            .data("item.autocomplete", item)
+            .append("<a>" + "<img style='width:40px;height:40px' src='" + item.image + "' /> " + item.label + "</a>")
+            .appendTo(ul);
+    };
+});
+
+//$(document).ready(function () {
+//    $("#PrepaidRecharge1").change(function () {
+//        debugger;
+//        $('#txtOperator').val('');
+//    });
+//    $("#PostpaidRecharge2").change(function () {
+
+//        $('#txtOperator').val('');
+//    });
+//});
+
+//var GetElectricityBillDetails = function () {    
+function GetElectricityBillDetails() {
+    $('#divElecBillDisplay').show();
+    $('#Electricityprogress').show();
+    var _accountNo = $('#CustomerId').val();
+    var _mobileNo = $('#MobileNo').val();
+    var _geoLocation = $('#GeolocationElectricity').val();
+    var Service_Key = $('#hfElectricityperator').val();
+    var UnitNo = "";
+    var CityName = $('#txtCityName').val();
+    if (Service_Key == "MDE") {
+        UnitNo = $('#BillUnitData').val();
+    }
+    else { UnitNo = ""; }
+    $.ajax({
+        type: "POST",
+        url: "/Merchant/MerchantRechargeService/GetBillInformation",
+        data: { AccountNo: _accountNo, MobileNo: _mobileNo, GeoLocation: _geoLocation, ServiceKey: Service_Key, Unitno: UnitNo, CityName: CityName },
+        success: function (data) {
+            if (data == "Unauthorized Access") {
+                bootbox.alert({
+                    message: data,
+                    backdrop: true
+                });
+                $('#Electricityprogress').hide();
+            }
+            else {
+                var Electricitybill = JSON.parse(data);
+                debugger;
+                var status = Electricitybill.statuscode;
+                if (status === "TXN") {                    
+                    //document.getElementById("RechargeAmt").innerHTML = JSON.stringify(Electricitybill.data.dueamount);
+                    var amount = Electricitybill.data.dueamount;
+                    var elec_Ref = Electricitybill.data.reference_id;
+                    $('#BillAMount').val(amount);
+                    $('#ELECreferenceID').val(elec_Ref);
+                    $('#DueDate').html(Electricitybill.data.duedate);
+                    $('#CustomerName').html(Electricitybill.data.customername);
+                    $('#txtElecbillnumber').html(Electricitybill.data.billnumber);
+                    $('#txtElectricityRefNo').val(Electricitybill.data.reference_id);
+                    $('#btnEleDisplay').hide();
+                    $('#btnsubmitElectricity').show();
+                    $('#Errormsg').html('');
+
+                }
+                else {
+                    $('#Errormsg').html(Electricitybill.status);
+                }
+                $('#Electricityprogress').hide();
+            }
+        }
+    })
+}
---
+++ WHITELABEL.Web/obj/Release/Package/PackageTmp/HelperJS/LoginJs.js
@@ -0,0 +1,104 @@
+﻿$('#txtMerchantemailaddress').on('input blur change keyup', function () {
+    if ($(this).val().length != 0) {
+        var token = $(':input[name="__RequestVerificationToken"]').val();
+        $.ajax({
+            url: "/Login/CheckEmailAvailability",
+            //url: "@Url.Action("CheckEmailAvailability", "Login")",
+            data: {
+                __RequestVerificationToken: token,
+                emailid: $(this).val()
+            },
+            cache: false,
+            type: "POST",
+            success: function (data) {
+                // DONE
+                if (data.result == "available") {
+                    $('#txtMerchantemailaddress').css('border', '3px #090 solid');
+                    $('#btnMersubmit').attr('disabled', false);
+                }
+                else {
+                    $('#txtMerchantemailaddress').css('border', '3px #C33 solid');
+                    $('#btnMersubmit').attr('disabled', true);
+                    //alert("This email id is already registered");
+                }
+            },
+            error: function (xhr, status, error) {
+                console.log(xhr.responseText);
+                alert("message : \n" + "An error occurred" + "\n status : \n" + status + " \n error : \n" + error);
+            }
+        });
+    }
+    else {
+        $('#btnMersubmit').attr('disabled', true);
+    }
+});
+
+$('#txtDistemailaddress').on('input blur change keyup', function () {
+
+    if ($(this).val().length != 0) {
+        var token = $(':input[name="__RequestVerificationToken"]').val();
+        $.ajax({
+            url: "/Login/CheckEmailAvailability",
+            //url: "@Url.Action("CheckEmailAvailability", "Login")",
+            data: {
+                __RequestVerificationToken: token,
+                emailid: $(this).val()
+            },
+            cache: false,
+            type: "POST",
+            success: function (data) {
+                // DONE                        
+                if (data.result == "available") {
+                    $('#txtDistemailaddress').css('border', '3px #090 solid');
+                    $('#btnDistsubmit').attr('disabled', false);
+                }
+                else {
+                    $('#txtDistemailaddress').css('border', '3px #C33 solid');
+                    $('#btnDistsubmit').attr('disabled', true);
+                    //alert("This email id is already registered");
+                }
+            },
+            error: function (xhr, status, error) {
+                console.log(xhr.responseText);
+                alert("message : \n" + "An error occurred" + "\n status : \n" + status + " \n error : \n" + error);
+            }
+        });
+    }
+    else {
+        $('#btnDistsubmit').attr('disabled', true);
+    }
+});
+
+$(document).ready(function () {
+    $("#txtDistributorMemberaName").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: '/Login/GetDistributorMemberName',
+                data: "{ 'prefix': '" + request.term + "'}",
+                dataType: "json",
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return item;
+                    }))
+                },
+                error: function (response) {
+                    alert(response.responseText);
+                },
+                failure: function (response) {
+                    alert(response.responseText);
+                }
+            });
+        },
+        focus: function (event, ui) {
+            $('#txtDistributorMemberaName').val(ui.item.label);
+            return true;
+        },
+        select: function (e, i) {
+            $("#hfDistributorId").val(i.item.val);
+            return true;
+        },
+        minLength: 1
+    });
+});
