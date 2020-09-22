afa58efe2542195f25788b7b6c3d061b08b50a0e
rasabless2020 <rahuls1017@gmail.com>
2020-09-22 20:40:48 +0530

    Add New File

---
+++ WHITELABEL.Data/Models/TBL_PAYMENT_GATEWAY_RESPONSE.cs
@@ -0,0 +1,17 @@
+﻿namespace WHITELABEL.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    [Table("PAYMENT_GATEWAY_RESPONSE")]
+    public class TBL_PAYMENT_GATEWAY_RESPONSE
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ID { get; set; }
+        public long MEM_ID { get; set; }
+        public string EASEBUZZ_RESPONSE { get; set; }
+        public DateTime? EXECUTE_DATE { get; set; }
+        public string STATUS { get; set; }
+    }
+}
